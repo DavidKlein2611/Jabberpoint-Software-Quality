4f0e5092e32174269477bcc6595284ee1acc3a9b
David Klein <davidklein2611@gmail.com>
2026-03-24 10:38:33 +0100

    Fix Checkstyle violations

--- src/main/java/com/nhlstenden/jabberpoint/command/Command.java
+++ src/main/java/com/nhlstenden/jabberpoint/command/Command.java
@@ -2,8 +2,10 @@ package com.nhlstenden.jabberpoint.command;
 
 /**
  * Applied SOLID Principles via the Command Pattern:
- * - Single Responsibility Principle (SRP): Each implementation of Command only handles the execution of one specific action.
- * - Open/Closed Principle (OCP): We can add new Commands to the application without modifying existing command-handling code.
+ * - Single Responsibility Principle (SRP): Each implementation of Command only
+ * handles the execution of one specific action.
+ * - Open/Closed Principle (OCP): We can add new Commands to the application
+ * without modifying existing command-handling code.
  */
 public interface Command
 {
