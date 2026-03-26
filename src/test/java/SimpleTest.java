75392927d705b814498499c4790bf4c27846db37
David Klein <davidklein2611@gmail.com>
2026-03-26 08:55:19 +0100

    Add model unit tests; remove SimpleTest

--- src/test/java/SimpleTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-class SimpleTest {
-    @Test
-    void aSimpleTest() {
-        assertTrue(true, "This is a simple placeholder test to ensure the test framework runs.");
-    }
-}
